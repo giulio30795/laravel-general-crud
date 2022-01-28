e00c9273c2a8f28d4651519d20e9b9253ea0ae47
giulio30795 <giulioincarbone95@gmail.com>
2022-01-28 18:07:34 +0100

    index-style 1

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -15,7 +15,8 @@ class ServiceController extends Controller
      */
     public function index()
     {
-        return view('services.index');
+        $services = Service::all();
+        return view('services.index', compact('services'));
     }
 
     /**
--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -1,8 +1,47 @@
 @extends('layout.main')
 
 @section('content')
-    <button class="btn btn-success">
-        <a href="{{route('services.create')}}"></a>
-        Aggiungi Servizio</button>
+    <div class="container">
+        <h1>Dashboard Servizi sito DoulaCristina</h1>
+        <button class="btn btn-success">
+            <a href="{{route('services.create')}}"></a>
+            Aggiungi Servizio
+        </button>
+        
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th colspan="3">Actions</th>
+                    </tr>
+                    @foreach ($services as $service)
+                    <tr>
+                        <td>
+                            {{ $service->id }}
+                        </td>
+                        <td>
+                            {{ $service->name }}
+                        </td><td>
+                            {{ $service->price }}
+                        </td>
+                        <td>
+                            <button class="btn btn-primary">
+                                View Details
+                            </button>
+                            <button class="btn btn-warning mx-2">
+                                Edit
+                            </button>
+                            <button class="btn btn-danger">
+                                Delete
+                            </button>
+                        </td>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
