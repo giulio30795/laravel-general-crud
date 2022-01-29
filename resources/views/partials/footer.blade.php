a197ffa96d78d69f5f9114045b3aefcd43e960f9
giulio30795 <giulioincarbone95@gmail.com>
2022-01-29 13:10:44 +0100

    style part 1

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,7 @@
 <footer>
-    Website Made by Giulio - Boolean Class #45
+
+    <img src="http://doulacristina.it/img/zeimusu-Swash-ornament.941d3490.svg" alt="">
+    <h3 class=" py-3 text-center">
+        Dashboard Made By Giulio - Boolean Clas#45 
+    </h3>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,28 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">Doula-Cristina Dashboard</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="/">
+            <img class="logo-home" src="http://doulacristina.it/img/1640562992772.dd45c549.jpg" alt="Logo">
+            </a>
+            <button class="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link text-black fs-4" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('services.index') }}">Servizi</a>
+                        <a class="nav-link text-black fs-4" href="{{ route('services.index') }}">Servizi</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('services.create') }}">Aggiungi Servizi</a>
+                        <a class="nav-link text-black fs-4" href="{{ route('services.create') }}">Aggiungi Servizi</a>
                     </li>
                 </ul>
             </div>
