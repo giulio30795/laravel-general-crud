ba58c034c88633f961326d39194a590bd0207b1e
giulio30795 <giulioincarbone95@gmail.com>
2022-01-29 11:40:40 +0100

    check

--- resources/views/services/edit.blade.php
+++ resources/views/services/edit.blade.php
@@ -1,5 +1,33 @@
 @extends('layout.main')
 
 @section('content')
-    Edit
+<div class="container py-5">
+    <h1>Modifica il Servizio</h1>
+    <form action="{{route('services.update', $service->id)}}" method="POST">
+        @csrf
+        @method('PATCH')
+        <div>
+            <label for="name" class="form-label">Name</label>
+            <input type="text" id="name" name="name" class="form-control" value="{{$service->name}}">
+            
+        </div>
+
+        <div>
+            <label for="description" class="form-label">Description</label>
+            <textarea id="description" name="description" class="form-control">{{$service->description}}</textarea>
+        </div>
+
+        <div>
+            <label for="image" class="form-label">Image</label>
+            <input type="text" id="image" name="image" class="form-control" value="{{$service->image}}">
+        </div>
+
+        <div>
+            <label for="price" class="form-label">Price</label>
+            <input type="text" id="price" name="price" class="form-control" value="{{$service->price}}">
+        </div>
+
+        <button type="submit" class=" mt-5 btn btn-primary">Modifica Servizio</button>
+    </form>
+</div>
 @endsection
