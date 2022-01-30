a523f3a1d0d56ef0cd5da3506287e502b78a5390
giulio30795 <giulioincarbone95@gmail.com>
2022-01-30 12:11:01 +0100

    style check

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,15 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="container flex-grow-1">
-        MY HOMEPAGE
+    <div class="container flex-grow-1 pt-4">
+        <h1>Benvenenuta nel sistema di Gestione dei servizi del tuo sito DoulaCristina
+        </h1>
+        <h2 class="py-4">una volta entrata potrai:</h2>
+        <ul class="fs-3">
+            <li>Visionare tutti i servizi presenti sul tuo sito</li>
+            <li>Aggiungere un nuovo servizio con relativo prezzo, descrizione e foto</li>
+            <li>Modificare un servizio già presente sul sito</li>
+            <li>Eliminare un servizio</li>
+        </ul>
     </div>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
-<footer>
+<footer class="flex-end">
 
     <img src="http://doulacristina.it/img/zeimusu-Swash-ornament.941d3490.svg" alt="">
-    <h3 class=" py-3 text-center m-0">
+    <h4 class=" text-center m-0">
         Dashboard Made By Giulio - Boolean Class #45 
-    </h3>
+    </h4>
 </footer>
